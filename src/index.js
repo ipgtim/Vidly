49ba4bbd022db2895db94929506c81f2ea10c68e
Tim Gorer <timogorer@gmail.com>
2019-07-25 14:27:38 -0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css"; // both libraries installed under node modules folder
+// when we build this app, webpack which is toolt hat bundles JS and CSS files will pull these CSS files and put them in our final bundle.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
