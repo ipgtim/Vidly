eb99cfbedecd833e711f2d09163341e3329217de
Tim Gorer <timogorer@gmail.com>
2019-07-29 00:28:44 -0700

    implemented validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -7,6 +7,7 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  // validates entire form
   validate = () => {
     const errors = {};
     const { account } = this.state;
@@ -31,10 +32,28 @@ class LoginForm extends Component {
     console.log("submitted");
   };
 
+  // validates one property (one input)
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+      // ...
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+      // ...
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
